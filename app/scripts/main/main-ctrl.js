cd01d9a4bcdaeed61607f94dde77d54248ec667f
Theophile VILLARD <t.villard@Dailymotions-MacBook-Pro-3.local>
2014-07-20 15:11:03 +0200

    Displaying list of hard-coded transactions

--- app/scripts/main/main-ctrl.js
+++ app/scripts/main/main-ctrl.js
@@ -2,4 +2,14 @@
 
 angular.module('wallet')
   .controller('MainCtrl', function ($scope) {
+
+    $scope.grandTotal = 45;
+
+    var transactions = [];
+    for (var i = 0; i < 10; i++) {
+      transactions.push({date: new Date(), amount: i + Math.random(), type: (i % 3 ? 'add' : 'remove')});
+    };
+
+    $scope.transactions = transactions;
+
   });
